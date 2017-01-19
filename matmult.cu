f1860d9e9b4d997a940ebd36062ae0ce3af01ecd
s152144 <cornelius.a.r@gmail.com>
2017-01-19 15:46:52 +0100

    working gpu3

--- matmult.cu
+++ matmult.cu
@@ -33,11 +33,35 @@ void matmult_gpu2_compute(int m,int n,int k,double *d_A,double *d_B,double *d_C)
     //printf("x = %i, y=%i, i=%i m=%i n=%i\n", x, y, i, m, n);
     d_C[i] = 0.0;
     for (l = 0; l < k; l++) {
-      int A_pos = y*k + l;
-      int B_pos = l*n + x;
-      d_C[i] += d_A[A_pos] * d_B[B_pos];
+      d_C[i] += d_A[y*k + l] * d_B[l*n + x];
     }
   }
+}
+
+__global__
+void matmult_gpu3_compute(int m,int n,int k,double *d_A,double *d_B,double *d_C) {
+  int i, l; 
+  int x = (blockIdx.x * blockDim.x + threadIdx.x)*2;
+  int y = blockIdx.y * blockDim.y + threadIdx.y; 
+  i = n * y + x;
+  
+  if(y < m) {
+    if(x < n) {
+      d_C[i] = 0.0;
+      for (l = 0; l < k; l++) {
+        d_C[i] += d_A[y*k + l] * d_B[l*n + x];
+      }
+
+      x += 1;
+      if(x < n) {
+        i += 1;
+        d_C[i] = 0.0;
+        for (l = 0; l < k; l++) {
+          d_C[i] += d_A[y*k + l] * d_B[l*n + x];
+        }
+      }
+    }
+  } 
 }
 
 extern "C" {
@@ -46,7 +70,7 @@ extern "C" {
 #include <stdlib.h>
 
 #ifndef GPU_DEVICE
-#define GPU_DEVICE 3
+#define GPU_DEVICE 2
 #endif
 
 void matmult_nat(int m,int n,int k,double *A,double *B,double *C) {
@@ -72,7 +96,7 @@ void matmult_lib(int m, int n, int k, double *A, double *B, double *C) {
 }
 
 void matmult_gpu1(int m,int n,int k,double *h_A,double *h_B,double *h_C) {
-  //cudaSetDevice(GPU_DEVICE);
+  cudaSetDevice(GPU_DEVICE);
   double* d_A;
   double* d_B;
   double* d_C;
@@ -109,7 +133,7 @@ void matmult_gpu1(int m,int n,int k,double *h_A,double *h_B,double *h_C) {
 }
 
 void matmult_gpu2(int m,int n,int k,double *h_A,double *h_B,double *h_C) {
-  cudaSetDevice(2);
+  cudaSetDevice(GPU_DEVICE);
   double* d_A;
   double* d_B;
   double* d_C;
@@ -142,6 +166,51 @@ void matmult_gpu2(int m,int n,int k,double *h_A,double *h_B,double *h_C) {
   dim3 dimBlock(block_x, block_y, 1);
   printf("gpu2: running\n");
   matmult_gpu2_compute<<<dimGrid, dimBlock>>>(m, n, k, d_A, d_B, d_C);
+  cudaDeviceSynchronize();
+
+  //TRANSFER DEVICE->HOST
+  cudaMemcpy (h_C, d_C, size_C, cudaMemcpyDeviceToHost);
+
+  //FREE DEVICE
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
+}
+
+void matmult_gpu3(int m,int n,int k,double *h_A,double *h_B,double *h_C) {
+  cudaSetDevice(GPU_DEVICE);
+  double* d_A;
+  double* d_B;
+  double* d_C;
+  int size_A = m*k*sizeof(double);
+  int size_B = n*k*sizeof(double);
+  int size_C = m*n*sizeof(double);
+
+  //ALLOCACE DEVICE
+  cudaMalloc ((void**) &d_A, size_A); 
+  if (d_A == NULL) { fprintf(stderr, "device memory allocation failed!\n"); return; }
+
+  cudaMalloc ((void**) &d_B, size_B); 
+  if (d_B == NULL) { fprintf(stderr, "device memory allocation failed!\n"); return; }
+
+  cudaMalloc ((void**) &d_C, size_C); 
+  if (d_C == NULL) { fprintf(stderr, "device memory allocation failed!\n"); return; }
+
+  //TRANSFER HOST->DEVICE
+  cudaMemcpy (d_A, h_A, size_A, cudaMemcpyHostToDevice);
+  cudaMemcpy (d_B, h_B, size_B, cudaMemcpyHostToDevice);
+  int factor = 16;
+
+  int grid_x = n/(factor*2) + 1;
+  int grid_y = m/(factor*1) + 1;
+
+  int block_x = factor;
+  int block_y = factor;
+  
+  dim3 dimGrid(grid_x, grid_y, 1);
+  dim3 dimBlock(block_x, block_y, 1);
+  printf("gpu3: running %d\n",k);
+  matmult_gpu3_compute<<<dimGrid, dimBlock>>>(m, n, k, d_A, d_B, d_C);
   cudaDeviceSynchronize();
 
   //TRANSFER DEVICE->HOST
